42c93f14d1c2f1e503a0e4dc7cfd4d01c88c4bcc
Gonzalo Asis <gonzaasis17@gmail.com>
2023-01-25 02:27:13 -0300

    estilos

--- src/Components/Main/ItemList/Item.jsx
+++ src/Components/Main/ItemList/Item.jsx
@@ -5,24 +5,23 @@ import "./Item.css";
 export const Item = (Products) => {
   return (
     <div>
-      <div className="card-container">
-        <h3>{Products.name}</h3>
-        <img
-          alt={`{Samsung ${Products.name}}`}
-          src={Products.image}
-          className="img"
-        />
-        <p>{`U$D ${Products.price}`}</p>
-        <p>{`${Products.memory} GB`}</p>
-
-        <Link className="ver-mas" to={`/products/${Products.id}`}>
-          Ver mas
-        </Link>
-
-        <button>+</button>
-      </div>
+      <Link className="ver-mas" to={`/products/${Products.id}`}>
+        <div className="card-container">
+          <h3>{Products.name}</h3>
+          <img
+            alt={`{Samsung ${Products.name}}`}
+            src={Products.image}
+            className="img"
+          />
+          <p>{`U$D ${Products.price}`}</p>
+          <p>{`${Products.memory} GB`}</p>
+        </div>
+      </Link>
     </div>
   );
 };
 
 export default Item;
+{
+  /* <button className="more-info-btn">More Info</button> */
+}
